ab45e40b21510df4259c74c52016de59fe9abbc0
pkindalov <panayotkindalov@gmail.com>
2016-09-21 01:20:10 +0300

    Write a JS function to calculate how many boxes will be needed to fit n bottles if each box fits k bottles

--- ControlFlowLogic/BoxesAndBottles.js
+++ ControlFlowLogic/BoxesAndBottles.js
@@ -1,6 +1,9 @@
 /**
  * Created by r3v3nan7 on 20.9.2016 г..
  */
+ 
+// Write a JS function to calculate how many boxes will be needed to fit n bottles if each box fits k bottles 
+ 
 function boxAndBottles([box, bottles]) {
     console.log(Math.ceil(box/bottles));
 }
