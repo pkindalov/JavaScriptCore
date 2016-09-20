afe673db156236254c201573b7f6ab0812ab8ebb
pkindalov <panayotkindalov@gmail.com>
2016-09-21 01:28:03 +0300

    Write a JS function to calculate cone volume and surface The cone height h and radius of the base r are given

--- ControlFlowLogic/ConeVolumeAndSurfaceArea.js
+++ ControlFlowLogic/ConeVolumeAndSurfaceArea.js
@@ -1,6 +1,10 @@
 /**
  * Created by r3v3nan7 on 20.9.2016 г..
  */
+ 
+/* Write a JS function to calculate cone volume and surface
+The cone height h and radius of the base r are given */
+ 
 function coneVolumeAndArea(input){
     let [r, h] = input.map(Number);
     let s = Math.sqrt(r * r + h * h);
