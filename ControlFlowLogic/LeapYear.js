f006be0b94dfec81c7c86b529fa4fe9b5d45cbf7
pkindalov <panayotkindalov@gmail.com>
2016-09-21 01:24:25 +0300

    Write a JS function to check whether a year is leap Can be divided to 4 and cannot be divided to 100 Or can be divided to 400

--- ControlFlowLogic/LeapYear.js
+++ ControlFlowLogic/LeapYear.js
@@ -2,6 +2,12 @@
  * Created by r3v3nan7 on 20.9.2016 г..
  */
 
+ 
+ /*Write a JS function to check whether a year is leap
+Can be divided to 4 and cannot be divided to 100
+Or can be divided to 400 */
+
+
 function leapYear(year) {
     let leap = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 );
     console.log(leap ? "yes" : "no" );
