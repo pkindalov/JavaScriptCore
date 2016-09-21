485f595e5093d7a001a35da723d8f42ab72e94b3
pkindalov <panayotkindalov@gmail.com>
2016-09-22 01:22:46 +0300

    Added solution of problem 1 - Sum 3 numbers

---
+++ IntroToJavaScript/Sum3Numbers.js
@@ -0,0 +1,19 @@
+/**
+ * Created by r3v3nan7 on 21.9.2016 г..
+ */
+// Function to Sum 3 Numbers
+
+function sum3Nums(inputNumbers) {
+    let num1 = Number(inputNumbers[0]);
+    let num2 = Number(inputNumbers[1]);
+    let num3 = Number(inputNumbers[2]);
+
+    let sum = num1 + num2 + num3;
+    console.log(sum);
+}
+
+
+sum3Nums(['1', '2', '3']);
+
+
+
