c042c50dda09a3dc9ca9eaacd73a059317ce8f70
pkindalov <panayotkindalov@gmail.com>
2016-09-23 10:20:07 +0300

    Write a JS function that can receive a name as input and print a greeting to the console. The input comes as array of strings with one element that is the name of the person. The output should be printed to the console.

---
+++ DataTypesExpressionsStatements/HelloJavaScript.js
@@ -0,0 +1,15 @@
+/**
+ * Created by r3v3nan7 on 22.9.2016 г..
+ */
+
+// Write a JS function that can receive a name as input and print a greeting to the console.
+//     The input comes as array of strings with one element that is the name of the person.
+//     The output should be printed to the console.
+
+
+function helloJavaScript([name]) {
+    console.log("Hello, " + name + ", I am JavaScript!");
+}
+
+
+helloJavaScript(['Bill Gates'])
