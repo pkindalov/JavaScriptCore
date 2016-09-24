02f70c13518d1eee22e575854166a56d2b77e94f
pkindalov <panayotkindalov@gmail.com>
2016-09-25 00:58:33 +0300

    Updated file GradsToDegrees.js Added one more solution.

--- DataTypesExpressionsStatements/GradsToDegrees.js
+++ DataTypesExpressionsStatements/GradsToDegrees.js
@@ -30,8 +30,22 @@ function gradsToDegrees([value]) {
 
 
     console.log(convert);
+
+
+
 }
 
+
+
+//second way
+// function gradsToDegrees(angleInGrade) {
+//     let grad = Number(angleInGrade[0]);
+//     let degrees = (grad * 0.9) % 360;
+//     if(degrees < 0){
+//         console.log(360 + degrees);
+//     }
+//     else console.log(degrees);
+// }
 
 
 gradsToDegrees(['-50']);
