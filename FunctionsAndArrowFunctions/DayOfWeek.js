f548d957e2fd098a5daa3e4ae5506c1bce0925a0
pkindalov <panayotkindalov@gmail.com>
2016-09-28 12:29:45 +0300

    Write a JS function to return the day number by day of week Example: Monday - 1, …, Sunday - 7, other - error

---
+++ FunctionsAndArrowFunctions/DayOfWeek.js
@@ -0,0 +1,28 @@
+/**
+ * Created by r3v3nan7 on 27.9.2016 г..
+ */
+
+function dayOfWeek(day) {
+    if(day == 'Monday'){
+        return 1;
+    } else if(day == 'Tuesday'){
+        return 2;
+    }else if(day == 'Wednesday'){
+        return 3;
+    }else if(day == 'Thursday'){
+        return 4;
+    }else if(day == 'Friday'){
+        return 5;
+    }else  if(day == "Saturday"){
+        return 6;
+    }else if(day == "Sunday"){
+        return 7;
+    }else {
+        return "error";
+    }
+
+
+}
+
+
+console.log(dayOfWeek("sdasda"));
