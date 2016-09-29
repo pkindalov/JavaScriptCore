17c920a28a8c237d533453902345d37cd832a0fc
pkindalov <panayotkindalov@gmail.com>
2016-09-29 22:10:34 +0300

    Write a JS program that receives data about a quiz and prints it formatted as an XML document. The data comes as pairs of question-answer entries.The input comes as an array of string elements. The output should be printed on the console.

---
+++ FunctionsAndArrowFunctions/03.TemplateFormat.js
@@ -0,0 +1,21 @@
+function templateFormat(input) {
+    console.log("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    console.log("\<quiz\>");
+    for(let i = 0;i<input.length;i+=2){
+        let question = input[i];
+        let answer = input[i+1];
+
+        console.log("\<question\>");
+        console.log(question);
+        console.log("\</question\>");
+        console.log("\<answer\>");
+        console.log(answer);
+        console.log("\</answer\>");
+    }
+    console.log("\</quiz\>")
+}
+
+templateFormat(["Dry ice is a frozen form of which gas?",
+    "Carbon Dioxide",
+    "What is the brightest star in the night sky?",
+    "Sirius"]);
